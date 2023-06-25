efc9b1ef9ddc32c4aee072e5e23b0620b9c37fad
marianacont <87699836+marianacont@users.noreply.github.com>
2023-06-25 08:49:09 -0300

    starting with api

--- scripts/fetchApi.js
+++ scripts/fetchApi.js
@@ -1,5 +1,6 @@
 const apiKey = "076125fe7d0647b6a94e0263bcf57f86";
+
 fetch(`https://rawg.io/api/games?token&key=${apiKey}`)
   .then(res => res.json())
-  .then(data => console.log(data))
+  .then(data => data.results.map(gameData => console.log(gameData.rating)))
   .catch(error => console.error('Error:', error));
