373bf111da60651fd14f8385376ff509d686319f
marianacont <87699836+marianacont@users.noreply.github.com>
2023-07-13 10:29:52 -0300

    changes at index js file

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,25 @@
-import { darkModeListener } from "./darkMode.js";
-import { showSearchInput } from "./showSearchInput.js";
+import { darkModeListener, windowListener } from "./dark-mode.js";
+import { showSearchInput } from "./show-search-input.js";
 import { changeColumnViewListener } from "./columns.js";
-import { createGameCards } from "./fetchApi.js";
-import { getGameDetailsAndRender } from "./nuevafuncionfetch.js";
+import { createGameCards } from "./fetch-api.js";
+import { logoutListener } from "./logout.js";
+import { getGameDescription } from "./fetch-api-details.js";
+import { searchGamesWithDebounce } from "./search-games.js";
+import { getGames } from "./new-function.js"
 
 // Dark-mode button
 const switchMode = document.querySelectorAll('.switch');
 // Lens (tablet screen)
 const lens = document.querySelector('.lens');
-//
 const colBtn = document.querySelectorAll("img[class*='col']")
-
+const logoutBtn = document.querySelectorAll('.logout');
+const searchInput = document.querySelector('.search-input')
 
 
 
 // Dark/light mode functionality
 switchMode.forEach(darkModeListener);
+windowListener()
 
 // Show search input (tablet screen)
 lens.addEventListener('click', showSearchInput);
@@ -27,4 +31,19 @@ colBtn.forEach(changeColumnViewListener)
 
 // fetch api
 createGameCards()
-// getGameDetailsAndRender();
+getGameDescription()
+
+
+// Logout
+logoutBtn.forEach(logoutListener)
+
+//Search games
+searchInput.addEventListener('input', (e) => {
+    // e.preventDefault();
+    let value = e.target.value
+    searchGamesWithDebounce(value)
+});
+
+// new functionality
+// getGames()
+
