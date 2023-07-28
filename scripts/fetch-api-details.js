90a62437f081932ba2a1ceb3a6a70a2daa04dcc9
marianacont <87699836+marianacont@users.noreply.github.com>
2023-07-28 16:06:09 -0300

    card modal functionality and cards functionality

--- scripts/fetch-api-details.js
+++ scripts/fetch-api-details.js
@@ -1,37 +1,40 @@
+
+
+
 export const getGameDescription = () => {
-  const apiKey = '7a29cc5de6ab485491bdb04ccd264f92'; 
+  const apiKey = 'b42d30de36764aaf933ab77eeea3f2a6'; 
   const url = `https://rawg.io/api/games?key=${apiKey}`;
 
 
   fetch(url)
     .then(res => res.json())
     .then(data => data.results)
-    .then(games => games.map(game => game.id))
-    .then(ids => ids.forEach(id => {
-            fetchCardsDetails(id)}))
+    .then(games => games.map(game => {fetchCardsDetails(game.id)}))
     .catch(error => console.error('Error:', error));
 
 
     const fetchCardsDetails = async (id) => {
         return fetch(`https://rawg.io/api/games/${id}?key=${apiKey}`)
                 .then(response => response.json())
-                .then(data => createDescriptionParagraph(data))
+                .then(data => {obtainGamesDetails(data)})
                 .catch(error => console.error(error))
     };
 
-    const createDescriptionParagraph = (game) => {
+    const obtainGamesDetails = (game) => {
         let cards = document.querySelectorAll(".card")
        
-                for(let card of cards){
-                        if(card.id == game.id){
-                                let card = document.getElementById(`${game.id}`)
-                                let p = card.querySelector('.card_description')
-                                let descriptionParagraph = game.description
-                                p.innerHTML = descriptionParagraph
-                        }
-        }
-        
+        for(let card of cards){
+            if(card.id == game.id){
+                const card = document.getElementById(`${game.id}`)
 
-    }
+                // obtain description
+                const p = card.querySelector('.card_description')
+                const descriptionParagraph = game.description
+                p.innerHTML = descriptionParagraph
+
+            };
+        };
+        
+    };
 
 }; 
--- scripts/fetch-api.js
+++ scripts/fetch-api.js
@@ -1,23 +1,25 @@
-let pageNumber = 1;
-const apiKey = "7a29cc5de6ab485491bdb04ccd264f92";
-const url = `https://rawg.io/api/games?key=${apiKey}&page=${pageNumber}`
-let gallery = document.querySelector(".gallery");
+import { createModalCard } from "./one-card-game.js";
 
-export const createGameCards = () => {  
-  
+let gallery = document.querySelector(".gallery");
+let pageNumber = 1
+
+export const createGameCards = (pageNumber) => {  
+    const apiKey = "b42d30de36764aaf933ab77eeea3f2a6";
+    const url = `https://rawg.io/api/games?key=${apiKey}&page=${pageNumber}`
+
   // fetch API
   fetch(url)
         .then(res => res.json())
         .then(data => data.results)
         .then(results => createCard(results))
         .catch(error => console.error('Error:', error));
-
 };
-  // Create card
+
+// Create card
  export const createCard = (games) => {
     let card = ''
 
-    games.map(async game =>{
+    games.map(game =>{
         // Get genres from each game
         let genres = game.genres.map(genre => {
             let genreList =  genre.name
@@ -29,46 +31,69 @@ export const createGameCards = () => {
 
         
         // Create card with all the information
-            card +=  `
+            card +=   `
             <div class="card" id="${game.id}">
-                <div class="card_image" style="background-image: url(${game.background_image})"></div>
-                <div class="card_content">
-                    <div class="flex">
-                        <p class="card_title">${game.name}</p>
-                        <p class="card_number">#${games.indexOf(game) + 1}</p>
-                    </div>
-                    
-                    <div class="flex">
-                        <p class="card_realease">Release date:</p>
-                        <p class="card_date">${game.released}</p>
+                <div class="modal_container">
+                    <div class="card_image" style="background-image: url(${game.background_image})">
+                        <div class="gradient_layer"></div>
                     </div>
 
-                    <div class="flex">
-                        <p class="card_genres">Genres:</p>
-                        <p class="card_genres_names">${genres}</p>
+                    <div class="card_content">
+                        <svg 
+                            class="hide card_close" 
+                            width="24" height="24"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="#fff">
+                            <path fill-rule="evenodd" clip-rule="evenodd" 
+                            d="M5.293 5.293a1 1 0 0 1 1.414 0L12 10.586l5.293-5.293a1 1 0 1 1 1.414 1.414L13.414 12l5.293 5.293a1 1 0 0 1-1.414 1.414L12 13.414l-5.293 5.293a1 1 0 0 1-1.414-1.414L10.586 12 5.293 6.707a1 1 0 0 1 0-1.414z" /></svg>
+            
+                        <div class="card_header flex">
+                            <h2 class="card_title">${game.name}</h2>
+                            <p class="card_number">#${games.indexOf(game) + 1}</p>
+                        </div>
+
+                        <div class="hide card_highlights">
+                            <p class="card_date">${game.released}</p>
+                            <p><span>#${games.indexOf(game) + 1}</span> top 2023</p>
+                            <p><span>#5</span> Action</p>
+                        </div>
+                        
+                        <div class="info flex">
+                            <p class="card_realease">Release date:</p>
+                            <p class="card_date">${game.released}</p>
+                        </div>
+
+                        <div class="info flex">
+                            <p class="card_genres">Genres:</p>
+                            <p class="card_genres_names">${genres}</p>
+                        </div>
+
+                        <div class="card_icons flex">
+                            ${platforms}
+                        </div>
+
+                        <p class="card_description">
+                        
+                        </p>
+
                     </div>
-
-                    <div class="card_icons flex">
-                        ${platforms}
-                    </div>
-
-                    <p class="card_description">
-                      
-                    </p>
-
                 </div>
             </div>`
 
-           gallery.innerHTML = card        
+               
     });   
+    gallery.insertAdjacentHTML('beforeend', card);
+
+    // Card modal
+createModalCard()
+
 };
 
 //Infinite scrolling
 window.addEventListener('scroll', (e) => {
     const { scrollTop, clientHeight, scrollHeight } = document.documentElement
-    if(scrollTop + clientHeight >= scrollHeight){
-        console.log('cargar más');
+    if(scrollTop + clientHeight >= scrollHeight ){
         pageNumber++
-        createGameCards()
+        createGameCards(pageNumber)
     }
-})
+});
