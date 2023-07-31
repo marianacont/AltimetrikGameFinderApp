f83dc7c213bb9bc592f221ec6114bc7a9e62240d
marianacont <87699836+marianacont@users.noreply.github.com>
2023-07-31 17:36:38 -0300

    fixing login bug

--- scripts/login/login.js
+++ scripts/login/login.js
@@ -5,30 +5,64 @@ const invalidPassword = document.querySelector('.invalid-password');
 
 
 // Function that find a match within the value of the input and the users
-const findUser = async () => {
+// const findUser = async () => {
 
+//     let data = {
+//         "email": inputEmail.value,
+//         "password": inputPass.value
+//     }
+
+//     fetch("http://localhost:3000/login", {
+//         method: 'POST',
+//         body: JSON.stringify(data),
+//         headers: {"Content-type": "application/json; charset=UTF-8"}
+//     })
+//     .then(res => res.json())
+//     .then(user => {
+//         // console.log(user);
+//         // // localStorage.setItem('user', user.accessToken)
+//         // // window.location.href = 'games.html'
+//     })
+//     .catch(err => {
+//         err.statusText
+//         invalidPassword.style.display = 'block';
+//         invalidPassword.innerText = 'Wrong email or password'
+//     })
+
+// }
+const findUser = async () => {
     let data = {
         "email": inputEmail.value,
         "password": inputPass.value
     }
 
-    fetch("http://localhost:3000/login", {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {"Content-type": "application/json; charset=UTF-8"}
-    })
-    .then(res => res.json())
-    .then(user => {
-        localStorage.setItem('user', user.accessToken)
-        window.location.href = 'games.html'
-    })
-    .catch(err => {
-        err.statusText
-        invalidPassword.style.display = 'block';
-        invalidPassword.innerText = 'Wrong email or password'
-    })
+    try {
+        const response = await fetch("http://localhost:3000/login", {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {"Content-type": "application/json; charset=UTF-8"}
+        });
 
+        if (response.ok) {
+            const user = await response.json();
+            // Realizar acciones con el usuario obtenido si es necesario
+            console.log(user);
+            localStorage.setItem('user', user.accessToken);
+            window.location.href = 'games.html';
+        } else if (response.status === 400) {
+            // El servidor respondió con un código 400 (Bad Request)
+            invalidPassword.style.display = 'block';
+            invalidPassword.innerText = 'Wrong email or password';
+        } else {
+            // Otros códigos de respuesta inesperados
+            console.error("Unexpected response status:", response.status);
+        }
+    } catch (error) {
+        // Error de red u otras excepciones
+        console.error("Error occurred:", error);
+    }
 }
+
 
 export const submitForm = () =>{
     if (inputEmail.value === ''){
